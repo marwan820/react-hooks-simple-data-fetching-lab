03ea5cd68dfbfda911dc24534db080770eb223dd
Marwan Rahim <marwan820@Marwan>
2023-08-27 22:05:01 -0500

    app is working, just cant pass tests yet

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
+
+
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+
+
+export default function App(){
+
+    const [dogImage,setDogImage] = useState('')
+    const [isLoaded,setisLoaded] = useState(false)
+
+
+    useEffect(() => {
+        
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => setDogImage(data.message))
+        
+         setisLoaded(true)
+        },[])
+
+       if (!isLoaded) return <p>Loading ...</p>
+
+        return(
+            <div>
+            <img  src={dogImage} alt="A Random Dog"></img>
+            </div>
+        
+        )
+
+
+
+
+
+
+}
